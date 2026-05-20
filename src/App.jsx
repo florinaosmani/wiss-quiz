d658c9fe1ed3d3bc452d497ac127c6a778843ccb
Florina Osmani <florina.osmani@hotmail.com>
2026-05-20 21:22:02 +0200

    aufgbae 4a`??

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,11 @@
 import './App.css';
-import Welcome from './Welcome';
-import Footer from './Footer';
-import Counter from './Counter';
+import GameSession from './GameSession';
 
 function App() {
   return (
     <div>
       <h1>Willkommen beim WISS-Quiz!</h1>
-      <p>Hier wird bald unser Quiz starten.</p>
-      <Welcome />
-      <Counter titel="Punktestand" startwert={0} schritt={1} />
-      <Counter titel="Lebenspunkte" startwert={100} schritt={10} />
-      <Counter titel="Geldbeutel (in CHF)" startwert={500} schritt={50} />
-      <Counter startwert={5} schritt={1} titel="Lautstärke" min={0} max={10} />
-      <Footer />
+      <GameSession />
     </div>
   );
 }
